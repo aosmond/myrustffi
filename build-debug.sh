cc328a4e0ffe7a1eee755ee01c39122b980429e9
Jeff Muizelaar <jrmuizel@gmail.com>
2019-07-03 14:25:04 -0400

    Make more portable

--- build-debug.sh
+++ build-debug.sh
@@ -1,2 +1,2 @@
 cargo build
-clang++-8 -O0 test.cpp -lmyrustffi -pthread -ldl -L ./target/debug/ -o ./target/debug/myrustffi
+clang++ -std=c++11 test.cpp -lmyrustffi -pthread -ldl -L ./target/debug/ -o ./target/debug/myrustffi
